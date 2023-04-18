bec073b0d3e3f7ea23a143bb4e8a2f7758c25c2a
Xiaowen Zhou <xzho0118@student.monash.edu>
2023-04-19 09:34:56 +1000

    CREATing tables.

--- Applied/App07/week7_schema.sql
+++ Applied/App07/week7_schema.sql
@@ -2,18 +2,21 @@
 Databases Week 7 Applied Class
 week7_schema.sql
 
-Author:
+Author: Xiaowen Zhou
 
 */
 
 -- 7.2
 -- DDL for Student-Unit-Enrolment model (using ALTER TABLE)
 --
-
+set echo on
+spool week7_schema_output.txt
 --
 -- Place DROP commands at head of schema file
 --
-
+drop table enrolment purge;
+drop table unit purge;
+drop table student purge;
 
 
 -- Create Tables
@@ -40,11 +43,13 @@ COMMENT ON COLUMN student.stu_dob IS
     'Student date of birth';
 
 /* Add STUDENT constraints here */
+ALTER TABLE student ADD CONSTRAINT student_pk PRIMARY KEY (stu_nbr);
+ALTER TABLE student ADD CONSTRAINT student_nbr_check check(stu_nbr > 10000000);
 
 /* Add UNIT data types here */
 CREATE TABLE unit (
-    unit_code   ,
-    unit_name   
+    unit_code   CHAR(7),
+    unit_name   VARCHAR2(50)
 );
 
 COMMENT ON COLUMN unit.unit_code IS
@@ -54,15 +59,17 @@ COMMENT ON COLUMN unit.unit_name IS
     'Unit name';
 
 /* Add UNIT constraints here */
+ALTER TABLE unit ADD CONSTRAINT unit_pk PRIMARY KEY (unit_code);
+ALTER TABLE unit ADD CONSTRAINT unit_name_unique UNIQUE(unit_name);
 
 /* Add ENROLMENT attributes and data types here */
 CREATE TABLE enrolment (
-    ,
-    ,
-    ,
-    ,
-    ,
-    
+    stu_nbr         NUMBER(8) NOT NULL,
+    unit_code       CHAR(7) NOT NULL,
+    enrol_year      NUMBER(4) NOT NULL,
+    enrol_semester  CHAR(1) NOT NULL,
+    enrol_mark      NUMBER(3),
+    enrol_grade     CHAR(2)
 );
 
 COMMENT ON COLUMN enrolment.stu_nbr IS
@@ -84,3 +91,23 @@ COMMENT ON COLUMN enrolment.enrol_grade IS
     'Enrolment grade (letter)';
 
 /* Add ENROLMENT constraints here */
+ALTER TABLE enrolment
+    ADD CONSTRAINT enrolment_pk PRIMARY KEY ( stu_nbr,
+                                              unit_code,
+                                              enrol_year,
+                                              enrol_semester );
+
+ALTER TABLE enrolment
+    ADD CONSTRAINT enrolment_student_pk FOREIGN KEY ( stu_nbr )
+        REFERENCES student ( stu_nbr );
+
+ALTER TABLE enrolment
+    ADD CONSTRAINT enrolment_unit_pk FOREIGN KEY ( unit_code )
+        REFERENCES unit ( unit_code );
+
+ALTER TABLE enrolment
+    ADD CONSTRAINT enrolment_sems_check CHECK ( enrol_semester IN ( '1', '2', '3' ) )
+    ;
+    
+spool off
+set echo off
