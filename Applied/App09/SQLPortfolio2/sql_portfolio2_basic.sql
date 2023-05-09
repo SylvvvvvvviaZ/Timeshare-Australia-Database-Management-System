549a3aaffa31f85580d59d00c3f70eff618b6bf3
Xiaowen Zhou <xzho0118@student.monash.edu>
2023-05-10 02:39:37 +1000

    Finish Q2.

--- Applied/App09/SQLPortfolio2/sql_portfolio2_basic.sql
+++ Applied/App09/SQLPortfolio2/sql_portfolio2_basic.sql
@@ -16,34 +16,57 @@ SPOOL sql_portfolio2_basic_output.txt
 -- ENSURE that your query is formatted and has a semicolon
 -- (;) at the end of this answer
 
-SELECT 
+SELECT
     m.maint_id,
     m.maint_desc,
     m.maint_cost,
     to_char(maint_datetime, 'dd-Mon-yyyy HH24:MI:SS') AS maint_date_time,
-    p.prop_address,   
-    o.owner_givname 
-    || ' ' 
+    p.prop_address,
+    o.owner_givname
+    || ' '
     || o.owner_famname AS owner_name
 FROM
-    rent.MAINTENANCE m
-    NATURAL JOIN PROPERTY p
-    NATURAL JOIN OWNER o 
+    rent.maintenance m
+    NATURAL JOIN property p
+    NATURAL JOIN owner    o
 WHERE
-    m.maint_cost BETWEEN 1000 AND 3000 
+    m.maint_cost BETWEEN 1000 AND 3000
     AND m.maint_assigned = 'Y'
 ORDER BY
-    m.maint_cost DESC, 
+    m.maint_cost DESC,
     m.maint_datetime DESC;
-
 
 /*2*/
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
 -- ENSURE that your query is formatted and has a semicolon
 -- (;) at the end of this answer
 
-
-
+SELECT
+    r.rent_agreement_no,
+    t.tenant_no, 
+    t.tenant_title 
+    || '. ' 
+    || t.tenant_givname 
+    || ' ' 
+    || t.tenant_famname AS tenant_name,
+    p.prop_address,
+    o.owner_givname
+    || ' ' 
+    || o.owner_famname AS owner_name, 
+    r.rent_lease_period 
+    || ' months' AS rent_lease_period 
+FROM
+    rent.rent r
+    JOIN rent.property p ON r.prop_no = p.prop_no 
+    JOIN rent.tenant t ON r.tenant_no = t.tenant_no 
+    JOIN rent.owner o ON p.owner_no = o.owner_no 
+WHERE 
+    r.rent_lease_start >= to_date('01-JAN-2022','dd-MON-yyyy')
+    AND r.rent_weekly_rate < 425 
+    AND r.rent_lease_period >= 6 
+ORDER BY 
+    r.rent_lease_period DESC, 
+    t.tenant_no ASC;
 
 --Comment out SET ECHO and SPOOL commands before submitting your portfolio
 SPOOL OFF
