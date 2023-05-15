42d08f930393c11d1690e708166db4760b19cc1c
Xiaowen Zhou <xzho0118@student.monash.edu>
2023-05-15 22:54:53 +1000

    Finish Q1.

--- Applied/App10/SQLPortfolio3/sql_portfolio3_intermediate.sql
+++ Applied/App10/SQLPortfolio3/sql_portfolio3_intermediate.sql
@@ -16,6 +16,25 @@ SPOOL sql_portfolio3_intermediate_output.txt
 -- ENSURE that your query is formatted and has a semicolon
 -- (;) at the end of this answer
 
+SELECT
+    tenant_no,
+    COUNT(dam_no) AS num_damages,
+    CONCAT(tenant_title, '. ', tenant_givname, ' ', tenant_famname) AS tenant_name,
+    CONCAT('$', CAST(SUM(dam_cost) AS DECIMAL(6, 2))) AS total_cost
+FROM
+    rent.TENANT t 
+    JOIN rent.DAMAGE d 
+    ON t.tenant_no = d.rent_agreement_no
+WHERE
+    to_char(d.dam_datetime, 'YYYY') = '2022'
+GROUP BY
+    tenant_no, 
+    tenant_name
+HAVING
+    SUM(d.dam_cost) > 0
+ORDER BY
+    total_cost DESC, 
+    tenant_no;
 
 /*2*/
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
