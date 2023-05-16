bab98c9be6f82dd3d1760c5739b7663010932269
Xiaowen Zhou <xzho0118@student.monash.edu>
2023-05-16 17:36:51 +1000

    Fix some error for Q1, can not use as tenant_name at GROUP BY.

--- Applied/App10/SQLPortfolio3/sql_portfolio3_intermediate.sql
+++ Applied/App10/SQLPortfolio3/sql_portfolio3_intermediate.sql
@@ -17,39 +17,35 @@ SPOOL sql_portfolio3_intermediate_output.txt
 -- (;) at the end of this answer
 
 
-Select * 
+--Select * From rent.RENT ;
 
-From rent.RENT ;
+--Select *  From rent.TENANT ;
 
-Select * 
-
-From rent.TENANT ;
-
-Select * 
-
-From rent.DAMAGE ;
+--Select *  From rent.DAMAGE ;
 
 
 SELECT
     t.tenant_no,
-    t.tenant_title 
-    || '. ' 
-    || t.tenant_givname 
-    || ' ' 
-    || t.tenant_famname AS tenant_name,    
-    COUNT(D.dam_no) AS num_of_damages,
-    CONCAT('$', FORMAT(SUM(D.dam_cost), 2)) AS total_cost
+    t.tenant_title
+    || '. '
+    || t.tenant_givname
+    || ' '
+    || t.tenant_famname AS tenant_name,
+    COUNT(d.dam_no)     AS num_of_damages,
+    to_char(SUM(d.dam_cost), '$9990.99') AS total_cost
 FROM
-    rent.TENANT t
-    JOIN rent.RENT R ON t.tenant_no = R.tenant_no
-    JOIN rent.DAMAGE D ON R.rent_agreement_no = D.rent_agreement_no
+         rent.tenant t
+    JOIN rent.rent   r ON t.tenant_no = r.tenant_no
+    JOIN rent.damage d ON r.rent_agreement_no = d.rent_agreement_no
 WHERE
     to_char(dam_datetime, 'YYYY') = '2022'
 GROUP BY
-    t.tenant_no, 
-    tenant_name
+    t.tenant_no,
+    tenant_title,
+    t.tenant_givname,
+    t.tenant_famname
 ORDER BY
-    total_cost DESC, 
+    total_cost DESC,
     t.tenant_no ASC;
 
 /*2*/
@@ -65,8 +61,7 @@ SELECT
     || ' ' 
     || t.tenant_famname AS tenant_name,
     r.prop_no,
-    p.prop_address,
-    COUNT(r.rent_agreement_no) AS num_rental_agreements
+    p.prop_address
 FROM
     rent.TENANT t
     JOIN rent.RENT r ON t.tenant_no = r.tenant_no
@@ -74,15 +69,20 @@ FROM
 WHERE
     p.prop_address = 'Tasmania'
 GROUP BY
-  t.tenant_no, tenant_name, r.prop_no, p.prop_address
+    t.tenant_no, 
+    t.tenant_title,
+    t.tenant_givname,
+    t.tenant_famname, 
+    r.prop_no, 
+    p.prop_address
 HAVING
-  COUNT(r.rent_agreement_no) > 1
+    COUNT(r.rent_agreement_no) > 1
 ORDER BY
-  t.tenant_no ASC;
+    t.tenant_no ASC;
 
 
 
 
 --Comment out SET ECHO and SPOOL commands before submitting your portfolio
--- SPOOL OFF
--- SET ECHO OFF
+SPOOL OFF
+SET ECHO OFF
