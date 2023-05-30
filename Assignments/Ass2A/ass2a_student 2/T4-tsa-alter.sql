afb411a90c03e8ef4c409dce189aaffe080f3b69
Xiaowen Zhou <xzho0118@student.monash.edu>
2023-05-31 05:18:31 +1000

    Modify the cabin table to add the new attribute "booking_count".

--- Assignments/Ass2A/ass2a_student 2/T4-tsa-alter.sql
+++ Assignments/Ass2A/ass2a_student 2/T4-tsa-alter.sql
@@ -1,20 +1,36 @@
 --*****PLEASE ENTER YOUR DETAILS BELOW*****
 --T4-tsa-alter.sql
 
---Student ID:
---Student Name:
---Unit Code:
---Applied Class No:
+--Student ID: 33049246
+--Student Name: Xiaowen Zhou
+--Unit Code: FIT2094
+--Applied Class No: Applied 01, Melbourne Wed 18:00
 
 /* Comments for your marker:
-
-
-
 
 */
 
 --4(a)
+ALTER TABLE cabin ADD booking_count NUMBER(8, 0) DEFAULT 0;
 
+UPDATE cabin c
+SET
+    booking_count = (
+        SELECT
+            COUNT(*)
+        FROM
+            booking b
+        WHERE
+            b.cabin_no = c.cabin_no
+    );
+
+DESC cabin;
+
+SELECT
+    cabin_no,
+    booking_count
+FROM
+    cabin;
 
 --4(b)
 
