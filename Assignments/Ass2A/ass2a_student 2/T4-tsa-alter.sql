1f824fb10fe4dd8d96e55e916cb0537313bf751a
Xiaowen Zhou <xzho0118@student.monash.edu>
2023-05-31 14:22:34 +1000

    add new table cleaning_status.

--- Assignments/Ass2A/ass2a_student 2/T4-tsa-alter.sql
+++ Assignments/Ass2A/ass2a_student 2/T4-tsa-alter.sql
@@ -35,13 +35,25 @@ FROM
     cabin;
 
 --4(b)
+
 DROP TABLE staff_role CASCADE CONSTRAINTS PURGE;
 
 CREATE TABLE staff_role (
-    role_id         CHAR(1) PRIMARY KEY,
-    role_name       VARCHAR2(20),
-    job_description VARCHAR2(100)
+    role_id         CHAR(1) NOT NULL,
+    role_name       VARCHAR2(20) NOT NULL,
+    job_description VARCHAR2(100) NOT NULL
 );
+
+COMMENT ON COLUMN staff_role.role_id IS
+    'Unique identifier for staff role';
+
+COMMENT ON COLUMN staff_role.role_name IS
+    'Name of the staff role';
+
+COMMENT ON COLUMN staff_role.job_description IS
+    'Description of the job responsibilities for the staff role';
+
+ALTER TABLE staff_role ADD CONSTRAINT staff_role_pk PRIMARY KEY ( role_id );
 
 INSERT INTO staff_role (
     role_id,
@@ -102,3 +114,56 @@ FROM
     ON staff.role_id = staff_role.role_id;
     
 --4(c)
+DROP TABLE cleaning_schedule CASCADE CONSTRAINTS PURGE;
+
+CREATE TABLE cleaning_schedule (
+    resort_id NUMBER(4) NOT NULL,
+    cabin_no NUMBER(3) NOT NULL,
+    cleaning_date DATE NOT NULL,
+    staff_id NUMBER(5) NOT NULL,
+    start_time DATE NOT NULL,
+    end_time DATE NOT NULL
+);
+
+COMMENT ON COLUMN cleaning_schedule.resort_id IS
+    'Resort identifier';
+
+COMMENT ON COLUMN cleaning_schedule.cabin_no IS
+    'Cabin number';
+
+COMMENT ON COLUMN cleaning_schedule.cleaning_date IS
+    'Date of cleaning';
+
+COMMENT ON COLUMN cleaning_schedule.staff_id IS
+    'Staff member ID';
+
+COMMENT ON COLUMN cleaning_schedule.start_time IS
+    'Start time of cleaning';
+
+COMMENT ON COLUMN cleaning_schedule.end_time IS
+    'End time of cleaning';
+
+ALTER TABLE cleaning_schedule
+    ADD CONSTRAINT cleaning_schedule_pk PRIMARY KEY ( resort_id,
+                                                      cabin_no,
+                                                      cleaning_date );
+
+ALTER TABLE cleaning_schedule
+    ADD CONSTRAINT fk_cleaning_resort FOREIGN KEY ( resort_id,
+                                                    cabin_no )
+        REFERENCES cabin ( resort_id,
+                           cabin_no );
+
+ALTER TABLE cleaning_schedule
+    ADD CONSTRAINT fk_cleaning_staff FOREIGN KEY ( staff_id )
+        REFERENCES staff ( staff_id );
+        
+-- Update the CABIN table structure 
+-- Include the new column cleaning_status indicating whether the cabin needs cleaning or not
+
+ALTER TABLE cabin ADD cleaning_status CHAR(1) DEFAULT 'N' NOT NULL;
+
+DESC cleaning_schedule;
+
+DESC cabin;
+
