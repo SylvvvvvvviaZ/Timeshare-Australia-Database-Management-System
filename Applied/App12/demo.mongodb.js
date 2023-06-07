6207c4667ac8d234d34f302e11666040844a28ae
Xiaowen Zhou <xzho0118@student.monash.edu>
2023-06-08 03:22:25 +1000

    Compare with solutions.

--- Applied/App12/demo.mongodb.js
+++ Applied/App12/demo.mongodb.js
@@ -1,43 +1,32 @@
-/* global use, db */
-// MongoDB Playground
-// To disable this template go to Settings | MongoDB | Use Default Template For Playground.
-// Make sure you are connected to enable completions and to be able to run a playground.
-// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
-// The result of the last command run in a playground is shown on the results panel.
-// By default the first 20 documents will be returned with a cursor.
-// Use 'console.log()' to print to the debug output.
-// For more documentation on playgrounds please refer to
-// https://www.mongodb.com/docs/mongodb-vscode/playgrounds/
+use ("xzho0118");
 
-// Select the database to use.
-use('mongodbVSCodePlaygroundDB');
+// db.getCollectionNames();
 
-// Insert a few documents into the sales collection.
-db.getCollection('sales').insertMany([
-  { 'item': 'abc', 'price': 10, 'quantity': 2, 'date': new Date('2014-03-01T08:00:00Z') },
-  { 'item': 'jkl', 'price': 20, 'quantity': 1, 'date': new Date('2014-03-01T09:00:00Z') },
-  { 'item': 'xyz', 'price': 5, 'quantity': 10, 'date': new Date('2014-03-15T09:00:00Z') },
-  { 'item': 'xyz', 'price': 5, 'quantity': 20, 'date': new Date('2014-04-04T11:21:39.736Z') },
-  { 'item': 'abc', 'price': 10, 'quantity': 10, 'date': new Date('2014-04-04T21:23:13.331Z') },
-  { 'item': 'def', 'price': 7.5, 'quantity': 5, 'date': new Date('2015-06-04T05:08:13Z') },
-  { 'item': 'def', 'price': 7.5, 'quantity': 10, 'date': new Date('2015-09-10T08:43:00Z') },
-  { 'item': 'abc', 'price': 10, 'quantity': 5, 'date': new Date('2016-02-06T20:20:13Z') },
-]);
+// db.studentenrolment.insertOne(
+//    { "_id":11443959,"name":"Geraldine Lomb","contactInfo":{"address":"55 Northwestern Trail, Toorak","phone":"4819717953","email":"Geraldine.Lomb@student.monash.edu"},"dob":"10-09-1996","enrolmentInfo":[{"unitcode":"FIT1045","unitname":"Algorithms and programming fundamentals in python","year":"2020","semester":2,"mark":39,"grade":"N"},{"unitcode":"FIT3157","unitname":"Advanced web design","year":"2021","semester":1,"mark":74,"grade":"D"},{"unitcode":"FIT2094","unitname":"Databases","year":"2021","semester":2,"mark":51,"grade":"P"},{"unitcode":"FIT2094","unitname":"Databases","year":"2021","semester":1,"mark":38,"grade":"N"},{"unitcode":"FIT1050","unitname":"Web fundamentals","year":"2020","semester":2,"mark":50,"grade":"P"}]},
+// );
 
-// Run a find command to view items sold on April 4th, 2014.
-const salesOnApril4th = db.getCollection('sales').find({
-  date: { $gte: new Date('2014-04-04'), $lt: new Date('2014-04-05') }
-}).count();
+// db.enrolment.insertMany([
+  //{"_id":11443959,"name":"Geraldine Lomb","contactInfo":{"address":"55 Northwestern Trail, Toorak","phone":"4819717953","email":"Geraldine.Lomb@student.monash.edu"},"dob":"10-09-1996","enrolmentInfo":[{"unitcode":"FIT1045","unitname":"Algorithms and programming fundamentals in python","year":"2020","semester":2,"mark":39,"grade":"N"},{"unitcode":"FIT3157","unitname":"Advanced web design","year":"2021","semester":1,"mark":74,"grade":"D"},{"unitcode":"FIT2094","unitname":"Databases","year":"2021","semester":2,"mark":51,"grade":"P"},{"unitcode":"FIT2094","unitname":"Databases","year":"2021","semester":1,"mark":38,"grade":"N"},{"unitcode":"FIT1050","unitname":"Web fundamentals","year":"2020","semester":2,"mark":50,"grade":"P"}]},
+  //{"_id":11620237,"name":"Marlane Joiris","contactInfo":{"address":"385 Warbler Road, Preston","phone":"5493750951","email":"Marlane.Joiris@student.monash.edu"},"dob":"29-05-1998","enrolmentInfo":[{"unitcode":"FIT1045","unitname":"Algorithms and programming fundamentals in python","year":"2020","semester":2,"mark":74,"grade":"D"},{"unitcode":"FIT3176","unitname":"Advanced database design","year":"2021","semester":2,"mark":93,"grade":"HD"},{"unitcode":"FIT3157","unitname":"Advanced web design","year":"2021","semester":1,"mark":null,"grade":"DEF"},{"unitcode":"FIT2094","unitname":"Databases","year":"2021","semester":1,"mark":33,"grade":"N"},{"unitcode":"FIT1050","unitname":"Web fundamentals","year":"2020","semester":2,"mark":57,"grade":"P"}]},
+  //{"_id":12489379,"name":"Gilberto Bwy","contactInfo":{"address":"5664 Loomis Parkway, Melbourne","phone":"7037621034","email":"Gilberto.Bwy@student.monash.edu"},"dob":"30-08-1992","enrolmentInfo":[{"unitcode":"FIT1045","unitname":"Algorithms and programming fundamentals in python","year":"2019","semester":1,"mark":40,"grade":"N"},{"unitcode":"FIT2094","unitname":"Databases","year":"2020","semester":1,"mark":63,"grade":"C"},{"unitcode":"FIT1050","unitname":"Web fundamentals","year":"2019","semester":1,"mark":44,"grade":"N"},{"unitcode":"FIT1050","unitname":"Web fundamentals","year":"2019","semester":2,"mark":92,"grade":"HD"},{"unitcode":"FIT1045","unitname":"Algorithms and programming fundamentals in python","year":"2019","semester":2,"mark":89,"grade":"HD"}]},
+  //{"_id":12511467,"name":"Francyne Rigney","contactInfo":{"address":"75 Buhler Street, Mulgrave","phone":"6994152403","email":"Francyne.Rigney@student.monash.edu"},"dob":"18-01-1992","enrolmentInfo":[{"unitcode":"FIT1045","unitname":"Algorithms and programming fundamentals in python","year":"2019","semester":1,"mark":87,"grade":"HD"},{"unitcode":"FIT3176","unitname":"Advanced database design","year":"2020","semester":1,"mark":null,"grade":"WH"},{"unitcode":"FIT3157","unitname":"Advanced web design","year":"2019","semester":2,"mark":98,"grade":"HD"},{"unitcode":"FIT2094","unitname":"Databases","year":"2019","semester":2,"mark":83,"grade":"HD"},{"unitcode":"FIT1050","unitname":"Web fundamentals","year":"2019","semester":1,"mark":86,"grade":"HD"}]},
+  //{"_id":12609485,"name":"Cassondra Sedcole","contactInfo":{"address":"6507 Tennessee Alley, Melbourne","phone":"8343944500","email":"Cassondra.Sedcole@student.monash.edu"},"dob":"07-08-1990","enrolmentInfo":[{"unitcode":"FIT1045","unitname":"Algorithms and programming fundamentals in python","year":"2019","semester":1,"mark":85,"grade":"HD"},{"unitcode":"FIT2094","unitname":"Databases","year":"2019","semester":2,"mark":69,"grade":"C"},{"unitcode":"FIT1050","unitname":"Web fundamentals","year":"2019","semester":1,"mark":23,"grade":"N"},{"unitcode":"FIT1050","unitname":"Web fundamentals","year":"2020","semester":1,"mark":44,"grade":"N"},{"unitcode":"FIT1050","unitname":"Web fundamentals","year":"2019","semester":2,"mark":36,"grade":"N"}]},
+  //{"_id":12802225,"name":"Friedrick Geist","contactInfo":{"address":"99271 Eliot Pass, Dingley","phone":"6787553656","email":"Friedrick.Geist@student.monash.edu"},"dob":"02-03-1997","enrolmentInfo":[{"unitcode":"FIT1045","unitname":"Algorithms and programming fundamentals in python","year":"2019","semester":1,"mark":83,"grade":"HD"},{"unitcode":"FIT3176","unitname":"Advanced database design","year":"2020","semester":1,"mark":null,"grade":"WH"},{"unitcode":"FIT3157","unitname":"Advanced web design","year":"2019","semester":2,"mark":62,"grade":"C"},{"unitcode":"FIT2094","unitname":"Databases","year":"2019","semester":2,"mark":52,"grade":"P"},{"unitcode":"FIT1050","unitname":"Web fundamentals","year":"2019","semester":1,"mark":61,"grade":"C"}]},
+  //{"_id":12842838,"name":"Herminia Mendus","contactInfo":{"address":"64186 East Lane, Moorabbin","phone":"4896374903","email":"Herminia.Mendus@student.monash.edu"},"dob":"25-04-1996","enrolmentInfo":[{"unitcode":"FIT1045","unitname":"Algorithms and programming fundamentals in python","year":"2019","semester":1,"mark":74,"grade":"D"},{"unitcode":"FIT3176","unitname":"Advanced database design","year":"2020","semester":1,"mark":null,"grade":"WH"},{"unitcode":"FIT3157","unitname":"Advanced web design","year":"2019","semester":2,"mark":71,"grade":"D"},{"unitcode":"FIT2094","unitname":"Databases","year":"2019","semester":2,"mark":58,"grade":"P"},{"unitcode":"FIT1050","unitname":"Web fundamentals","year":"2019","semester":1,"mark":76,"grade":"D"}]},
+  //{"_id":13019582,"name":"Tani Aitchison","contactInfo":{"address":"842 Paget Drive, Mount Waverley","phone":"7352456677","email":"Tani.Aitchison@student.monash.edu"},"dob":"16-07-1996","enrolmentInfo":[{"unitcode":"FIT1045","unitname":"Algorithms and programming fundamentals in python","year":"2020","semester":2,"mark":null,"grade":"WH"},{"unitcode":"FIT3157","unitname":"Advanced web design","year":"2021","semester":1,"mark":81,"grade":"HD"},{"unitcode":"FIT2094","unitname":"Databases","year":"2021","semester":1,"mark":42,"grade":"N"},{"unitcode":"FIT1050","unitname":"Web fundamentals","year":"2021","semester":2,"mark":47,"grade":"N"},{"unitcode":"FIT1050","unitname":"Web fundamentals","year":"2020","semester":2,"mark":20,"grade":"N"}]},
+  //{"_id":13028303,"name":"Herculie Mendus","contactInfo":{"address":"44 Becker Street, Mulgrave","phone":"2309618710","email":"Herculie.Mendus@student.monash.edu"},"dob":"02-08-1998","enrolmentInfo":[{"unitcode":"FIT1045","unitname":"Algorithms and programming fundamentals in python","year":"2019","semester":1,"mark":61,"grade":"C"},{"unitcode":"FIT3176","unitname":"Advanced database design","year":"2020","semester":1,"mark":null,"grade":null},{"unitcode":"FIT3157","unitname":"Advanced web design","year":"2019","semester":2,"mark":65,"grade":"C"},{"unitcode":"FIT2094","unitname":"Databases","year":"2019","semester":2,"mark":88,"grade":"HD"},{"unitcode":"FIT1050","unitname":"Web fundamentals","year":"2019","semester":1,"mark":97,"grade":"HD"}]},
+  //{"_id":13119134,"name":"Shandra Lindblom","contactInfo":{"address":"9241 Rieder Parkway, Chelsea","phone":"4384142213","email":"Shandra.Lindblom@student.monash.edu"},"dob":"20-04-2000","enrolmentInfo":[{"unitcode":"FIT1045","unitname":"Algorithms and programming fundamentals in python","year":"2019","semester":1,"mark":62,"grade":"C"},{"unitcode":"FIT3176","unitname":"Advanced database design","year":"2020","semester":1,"mark":null,"grade":null},{"unitcode":"FIT3157","unitname":"Advanced web design","year":"2019","semester":2,"mark":82,"grade":"HD"},{"unitcode":"FIT2094","unitname":"Databases","year":"2019","semester":2,"mark":64,"grade":"C"},{"unitcode":"FIT1050","unitname":"Web fundamentals","year":"2019","semester":1,"mark":91,"grade":"HD"}]}
+//  ]);
 
-// Print a message to the output window.
-console.log(`${salesOnApril4th} sales occurred in 2014.`);
+db.enrolment.find();
+db.enrolment.countDocuments();
 
-// Here we run an aggregation and open a cursor to the results.
-// Use '.toArray()' to exhaust the cursor to return the whole result set.
-// You can use '.hasNext()/.next()' to iterate through the cursor page by page.
-db.getCollection('sales').aggregate([
-  // Find all of the sales that occurred in 2014.
-  { $match: { date: { $gte: new Date('2014-01-01'), $lt: new Date('2015-01-01') } } },
-  // Group the total sales for each product.
-  { $group: { _id: '$item', totalSaleAmount: { $sum: { $multiply: [ '$price', '$quantity' ] } } } }
-]);
+db.enrolment.find({"_id":13119134});
+
+db.studentenrolment.find ({"enrolmentInfo. unitcode": "FIT3157"}, 
+{"_id": 1, "name": 1});
+
+db.studentenrolment.find({"$or": [{"contactInfo.address":/.*Mulgrave.*/}, 
+{"contactInfo.address":/.*Moorabbin.*/}]}, 
+{"_id": 0, "name": 1, "contactInfo. address": 1});
